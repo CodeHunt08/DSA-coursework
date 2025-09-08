4c769dcf02eb152ba953d53b55b68a7257961525
Parth <parthagre84@gmail.com>
2025-09-08 19:52:02 +0530

    Assignment No 1.cpp

--- Group 1/Assignment No 1.cpp
+++ Group 1/Assignment No 1.cpp
@@ -1,1 +1,35 @@
+//  You have been given an array of positive integers A1,A2,...,An with length N and you 
+//  have to print an array of same length(N) where the values in the new array are the sum of 
+//  every number in the array, except the number at that index. Write program in Python 
+//  Language. Write a C++ Program 
 
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"Enter the Size of an Array:- "<<endl;
+    cin>>n;
+    int arr[n],new_arr[n];
+
+
+    for(int i = 0;i<n;i++){
+        cout<<"Enter the Element "<<i+1<<endl;
+        cin>>arr[i];
+    }
+
+  
+    for(int i =0;i<n;i++){
+        sum += arr[i];
+
+    }
+
+    for(int i = 0;i<n;i++){ 
+        new_arr[i] = sum - arr[i];
+    }
+
+    cout<<"The new Array is:- "<<endl;
+    for(int i =0;i<n;i++){
+        cout<<new_arr[i]<<" ";
+    }
+return 0;
+}
